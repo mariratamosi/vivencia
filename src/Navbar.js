9ec7d48fad6ef55345eedbe16cd4a51c0bc42be5
marira <rongrazzo@gmail.com>
2022-04-22 18:57:35 +0600

    removed material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,52 +1,13 @@
 import { Link } from "react-router-dom"
-import {
-  AppBar,
-  Toolbar,
-  CssBaseline,
-  Typography,
-  makeStyles,
-} from "@material-ui/core"
-
-const useStyles = makeStyles((theme) => ({
-  navlinks: {
-    marginLeft: theme.spacing(10),
-    display: "flex",
-  },
-  logo: {
-    flexGrow: "1",
-    cursor: "pointer",
-  },
-  link: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "20px",
-    marginLeft: theme.spacing(20),
-    "&:hover": {
-      color: "yellow",
-      borderBottom: "1px solid white",
-    },
-  },
-}))
-
 function Navbar() {
-  const classes = useStyles()
   return (
-    <AppBar position="static">
-      <CssBaseline />
-      <Toolbar>
-        <Typography variant="h4" className={classes.logo}>
-          Navbar
-        </Typography>
-        <div className={classes.navlinks}>
-          <Link to="/" className={classes.link}>
-            Home
-          </Link>
-          <Link to="/infinity" className={classes.link}>
-            Infinity
-          </Link>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/infinity">Infinity</Link>
+      </div>{" "}
+      <div variant="h4">Vivencia</div>
+    </div>
   )
 }
 
