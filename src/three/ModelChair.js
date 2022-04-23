91bc20fb94581e5fe1f319a674feb5569c9afbdc
marira <rongrazzo@gmail.com>
2022-04-23 18:16:20 +0600

    color add

--- src/three/ModelChair.js
+++ src/three/ModelChair.js
@@ -1,23 +1,23 @@
 import { Canvas } from "@react-three/fiber"
 import { Html, useGLTF } from "@react-three/drei"
 import { Section } from "Utility/section"
-import { Suspense } from "react"
+import { Suspense, useEffect } from "react"
 
 const Model = () => {
   const gltf = useGLTF("/3d/armchairYellow.gltf")
   return <primitive object={gltf.scene} dispose={null} />
 }
 
-const HTMLContent = () => {
+const HTMLContent = ({ bgColor }) => {
+  useEffect(() => {
+    document.body.style.background = bgColor
+  }, [])
   return (
     <Section factor={1.5} offset={1}>
       <group position={[0, 250, 0]}>
         <mesh>
-          <Model position={[0, 35, 0]} />
+          <Model position={[0, 0, 0]} />
         </mesh>
-        <Html>
-          <div className="html-container">Hellooooooo</div>
-        </Html>
       </group>
     </Section>
   )
@@ -29,7 +29,7 @@ export default function ModelChair() {
       {/* TODO solve color issue */}
       <Canvas camera={{ position: [0, 0, 120], fov: 70 }}>
         <Suspense fallback={null}>
-          <HTMLContent />
+          <HTMLContent bgColor="#00FF00" />
         </Suspense>
       </Canvas>
     </div>
