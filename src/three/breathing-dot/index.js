0479a3916cabc69384a27fa46e68564299f03df5
marira <rongrazzo@gmail.com>
2022-05-03 02:58:53 +0600

    ow ripple

--- src/three/breathing-dot/index.js
+++ src/three/breathing-dot/index.js
@@ -5,7 +5,7 @@ import * as THREE from "three"
 
 function Dots() {
   const dotsRef = useRef()
-  const { vec, transform, positions } = useMemo(() => {
+  const { vec, transform, positions, distances } = useMemo(() => {
     const vec = new THREE.Vector3()
     const transform = new THREE.Matrix4()
     const positions = [...Array(10000)].map((_, i) => {
@@ -17,13 +17,22 @@ function Dots() {
       position.y += Math.random() * 0.3
       return position
     })
-    return { vec, transform, positions }
+    const distances = positions.map((pos) => pos.length())
+    return { vec, transform, positions, distances }
   }, [])
 
   useFrame(({ clock }) => {
-    const scale = 1 + Math.sin(clock.elapsedTime) * 0.3
+    const scale = roundedSquareWave(clock.elapsedTime, 0.001, 1, 0.5)
     for (let i = 0; i < 10000; ++i) {
-      vec.copy(positions[i]).multiplyScalar(scale)
+      const dist = distances[i]
+
+      // Distance affects the wave phase
+      const t = clock.elapsedTime - dist / 25
+
+      // Oscillates between -0.4 and +0.4 with period of 3.8 seconds
+      const wave = roundedSquareWave(t, 0.15 + (0.2 * dist) / 72, 0.4, 1 / 3.8)
+
+      vec.copy(positions[i]).multiplyScalar(wave + 1.3)
       transform.setPosition(vec)
       dotsRef.current.setMatrixAt(i, transform)
     }
@@ -35,6 +44,10 @@ function Dots() {
       <meshBasicMaterial />
     </instancedMesh>
   )
+}
+
+const roundedSquareWave = (t, delta, a, f) => {
+  return ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
 }
 
 export default function BreathingDots() {
