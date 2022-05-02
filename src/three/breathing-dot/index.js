b54d64900056310adf900b08f8f5d7294f5fb452
marira <rongrazzo@gmail.com>
2022-05-03 02:49:23 +0600

    dots moving raw sine

--- src/three/breathing-dot/index.js
+++ src/three/breathing-dot/index.js
@@ -1,30 +1,34 @@
 import { OrbitControls } from "@react-three/drei"
-import React, { useRef, useLayoutEffect } from "react"
-import { Canvas } from "react-three-fiber"
+import React, { useRef, useMemo } from "react"
+import { Canvas, useFrame } from "react-three-fiber"
 import * as THREE from "three"
 
 function Dots() {
   const dotsRef = useRef()
-  useLayoutEffect(() => {
-    // THREE.Matrix4 defaults to an identity matrix
+  const { vec, transform, positions } = useMemo(() => {
+    const vec = new THREE.Vector3()
     const transform = new THREE.Matrix4()
+    const positions = [...Array(10000)].map((_, i) => {
+      const position = new THREE.Vector3()
+      position.x = (i % 100) - 50
+      position.y = Math.floor(i / 100) - 50
+      position.y += (i % 2) * 0.5
+      position.x += Math.random() * 0.3
+      position.y += Math.random() * 0.3
+      return position
+    })
+    return { vec, transform, positions }
+  }, [])
 
-    // Apply the transform to the instance at index 0
-    //dotsRef.current.setMatrixAt(0, transform)
-
+  useFrame(({ clock }) => {
+    const scale = 1 + Math.sin(clock.elapsedTime) * 0.3
     for (let i = 0; i < 10000; ++i) {
-      let x = (i % 100) - 50
-      let y = Math.floor(i / 100) - 50
-      // Offset every other column (hexagonal pattern)
-      y += (i % 2) * Math.random() * 0.3
-
-      // Add some noise
-      x += Math.random() * 0.3
-      y += Math.random() * 0.3
-      transform.setPosition(x, y, 0)
+      vec.copy(positions[i]).multiplyScalar(scale)
+      transform.setPosition(vec)
       dotsRef.current.setMatrixAt(i, transform)
     }
-  }, [])
+    dotsRef.current.instanceMatrix.needsUpdate = true
+  })
   return (
     <instancedMesh args={[null, null, 10000]} ref={dotsRef}>
       <circleBufferGeometry args={[0.15]} />
@@ -35,7 +39,7 @@ function Dots() {
 
 export default function BreathingDots() {
   return (
-    <Canvas orthographic camera={{ zoom: 12 }}>
+    <Canvas orthographic camera={{ zoom: 20 }}>
       <color attach="background" args={["black"]} />
       <Dots />
       {/* <OrbitControls /> */}
